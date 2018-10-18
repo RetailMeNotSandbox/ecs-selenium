9f31245fd9a8b9e806b7b7e977509e517993d885
rrivera-rmn <rrivera@rmn.com>
2018-10-18 12:13:58 -0500

    Remove -host. Bump versions. Fix previous merge issues. (#16)
    * Reference implementation for Selenium on ECS.
    
    * Selenium containers version bump.
    
    * Having -host as an argument now causes problems.
    
    * Fix merge issues…

--- docker/common/ecs_entry_point.sh
+++ docker/common/ecs_entry_point.sh
@@ -20,7 +20,7 @@ set -o xtrace
 result="$(python /opt/bin/ecs-get-port-mapping.py)"
 eval "$result"
 
-export SE_OPTS="-host $EC2_HOST -remoteHost http://$EC2_HOST:$PORT_TCP_5555 -id $NODE_ID"
+export SE_OPTS="-remoteHost http://$EC2_HOST:$PORT_TCP_5555 -id $NODE_ID"
 
 # execute default extry_point.sh file
 /opt/bin/entry_point.sh
